b0268fe84102b139cf2f1d66d894b6ec5d1c4c12
abi-sabm <abigail.sabm@gmail.com>
2022-11-23 18:24:26 -0300

    Actualización de carpetas

--- JavaScript/Clase_4/Ejercicios/aterrizarAvion.js
+++
@@ -1,7 +0,0 @@
-// Aterrizar Avion - if ternario
-let velocidad = 270;
-let altura = 150;
-
-let puedeAterrizar = velocidad >= 268 && velocidad <= 278 && altura >= 150 && altura <= 300 ? true : false;
-
-puedeAterrizar == true ? console.log("Puede aterrizar") : console.log("No puede aterrizar");
